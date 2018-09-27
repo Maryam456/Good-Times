f143bdf60d1323edfe2348a1be6decb6a4fe300d
Maryam456 <37376125+Maryam456@users.noreply.github.com>
2018-09-27 13:20:49 -0600

    update

---
+++ index.js
@@ -0,0 +1,29 @@
+funtion simpleOperators (numberA, mnumerB){
+
+}
+
+let number = [4, 7];
+simpleOperators (numberA + numberB){
+let adding= number[0] + numberB;
+let subtract=  number[0] - numberB;
+let multiply = number[0] * numberB;
+let divide= number[0] / numberB;
+let skipCount= number[0] + number[1];
+console.log(adding);
+console.log(subtract);
+console.log(multiply);
+console.log(divide);
+}
+
+let number = [4, 7, 13, 99];
+simpleOperators(number[0], number[1]);
+  simpleOperators(number[2], number[3]);
+
+  let skipCount= 0; //skip count by 2's to 10
+  for(let i=1; i<=5, i++){
+    skipCount -=;
+    console.log(skipCount);
+
+
+
+  }
